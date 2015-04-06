0b85a0eb5841686e83efca0ed966d84686158a47
Michael Cramm <gmcramm@gmail.com>
2015-04-06 15:43:22 -0600

    Use query params

--- src/my_ring_app/app.clj
+++ src/my_ring_app/app.clj
@@ -1,6 +1,7 @@
 (ns my-ring-app.app)
 
 (defn handler [request]
-  {:status 200
-   :headers  {"Content-Type" "text/html"}
-   :body "Hello World"})
+  (let [subject (get-in request [:params "name"] "World")]
+    {:status 200
+     :headers  {"Content-Type" "text/html"}
+     :body (format "Hello %s" subject)}))
--- src/my_ring_app/core.clj
+++ src/my_ring_app/core.clj
@@ -1,9 +1,11 @@
 (ns my-ring-app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.params :refer [wrap-params]] ; NEW
             [my-ring-app.app :as app]
             [my-ring-app.middleware :refer [wrap-hello-translator]])
   (:gen-class))
 
 (defn -main [& args]
   (run-jetty (-> app/handler
+                 wrap-params ; NEW
                  wrap-hello-translator) {:port 3000}))
